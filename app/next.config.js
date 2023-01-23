f190113cf11313cb3c10e345f50cb66bd1774376
Thomas Hoang <megatunger@gmail.com>
2023-01-23 23:38:51 +0700

    update next config

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
